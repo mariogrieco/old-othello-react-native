aee99986d09327ff888084d20b3851afeee2885e
Mario <mariojosuexz@gmail.com>
2018-02-05 14:02:11 -0400

    initial

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,31 +6,16 @@ import {
   ImageBackground
 } from 'react-native'
 import Menu from '../../Menu/containers/MenuContainer'
-import Cell from '../../Board/container/CellContainer'
 
 class Home extends Component {
   render() {
     return (
-    <View style={styles.container}>
-      <Cell chip size={40} color='black' />
-      <Cell chip size={40} color='black' />
-      <Cell chip size={40} color='white' />
-      <Cell chip size={40} color='white' />
-      <Cell chip size={40} color='white' />
-    </View>
+      <Menu />
     )
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'red',
-    height: '100%',
-    width: '100%',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
 })
 
 export default Home
